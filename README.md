8cd349c7a38cdf08cd0e524f5cf55892176fbec7
salehelhalaby <35937335+salehelhalaby@users.noreply.github.com>
2023-08-20 20:48:07 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# CRUD Nodejs and Mysql
+This is a basic application crud that uses nodejs in the backend, mysql as database.
+
+# Useful Commands
+- to init mysql: `mysql -u root -p` or
+  ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'SetRootPasswordHere';
+
+# links
+- [bootstrap 4 theme](https://bootswatch.com/4/lux/bootstrap.min.css)
---
+++ database/db.sql
@@ -0,0 +1,25 @@
+
+-- create user --
+CREATE USER 'myuser'@'%' IDENTIFIED BY 'Password123@';
+GRANT ALL PRIVILEGES ON *.* TO 'myuser'@'%';
+FLUSH PRIVILEGES;
+
+-- to create a new database
+CREATE DATABASE customersdb;
+
+-- to use database
+use customersdb;
+
+-- creating a new table
+CREATE TABLE customer (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  phone VARCHAR(15)
+);
+
+-- to show all tables
+show tables;
+
+-- to describe table
+describe customer;
